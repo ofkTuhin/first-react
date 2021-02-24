a51f413a835f6fd8f3967767fdf9cddf5f9639db
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-24 16:28:06 +0600

    2nd

--- src/App.js
+++ src/App.js
@@ -2,17 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Product=[{
+    name:'tuhin',age:22
+  },{
+    name:'tuhin1',age:23
+  },
+  
+]
+  
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+    <Products name={Product[0]}></Products>
+    
+    <Person name="tuhin"></Person>
+    <Person name="tuhin"></Person>
+    <Person name="tuhin"></Person>
+    <Person name="tuhin"></Person>
         
       </header>
     </div>
   );
+}
+function Person(props){
+  const style={color:' blue',
+  backgroundColor:'green',
+  margin:10,
+  padding:5,
+  width:'150px'
+}
+  return(
+    <div style={style}><h1>{props.name}</h1>
+    <h3>i wanna be a jannati</h3></div>
+  )
+}
+
+function Products(props){
+  const productStyle={
+    backgroundColor:'blue',
+    color:'green',
+    width:'150px',
+    height:'200px',
+    marginTop:'20px'
+  }
+  return(
+    <div style={productStyle}>
+      <h2>{props.name.name}</h2>
+      <button>tuhin</button>
+    </div>
+  )
 }
 
 export default App;
